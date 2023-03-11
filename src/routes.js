78a7259de683e6c5b624fdfabce64fdcb59f8906
Yuwa Ko Ko <yuwakoko9898@gmail.com>
2023-03-12 01:57:15 +0630

    first commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const TestCode = React.lazy(() => import('./views/menus/testCode/TestCode'));
+const NewMenu = React.lazy(() => import('./views/menus/newMenu/NewMenu'));
+
+const routes = [
+  { path: '/', exact: true, name: 'Home' },
+  // { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/menus/TestCode', name: 'TestCode', component: TestCode },
+  { path: `/menus/NewMenu`, name: 'NewMenu', component: NewMenu}
+];
+
+export default routes;
+
+
+
